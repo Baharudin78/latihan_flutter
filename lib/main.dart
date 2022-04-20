1fa7490aca9249169fbfe60132693c2795baa0ec
Baharudin78 <bencoxgame78@gmail.com>
2022-04-20 08:46:15 +0700

    adding class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:ui';
-
+import 'package:latihan_flutter/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,62 +7,19 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: ListView(
-            padding: EdgeInsets.symmetric(vertical: 30.0),
-            children: <Widget>[
-              // Padding(
-              //   padding: EdgeInsets.only(left: 20.0, right: 140.0),
-              Row(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.only(left: 20.0),
-                        child: Text(
-                          "Hello Jeremi !",
-                          style: TextStyle(
-                              fontSize: 25.0, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 20),
-                        child: Text(
-                          "Which electric car would you like to rent",
-                          style: TextStyle(
-                              fontSize: 18.0, fontWeight: FontWeight.normal),
-                        ),
-                      )
-                    ],
-                  ),
-                  Column(
-                    // ignore: prefer_const_literals_to_create_immutables
-                    children: [
-                      const Image(
-                        image: AssetImage("assets/man.png"),
-                        width: 60,
-                        height: 60,
-                      )
-                    ],
-                  )
-                ],
-              ),
-              const Padding(
-                padding: EdgeInsets.only(left: 20, top: 50),
-                child: Text(
-                  "Most Popular",
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                ),
-              )
-            ],
-          ),
-        ),
+      title: "Flutter Travel UI",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFF3EBACE),
+        accentColor: Color(0xFFD8ECF1),
+        scaffoldBackgroundColor: Color(0xFFF3F5F7),
       ),
+      home: HomeScreen(),
     );
   }
 }
